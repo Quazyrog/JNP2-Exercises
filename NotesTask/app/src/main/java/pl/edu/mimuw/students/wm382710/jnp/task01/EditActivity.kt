32084d45a799736f66f4a232126e7e80279ebb01
Wojciech Matusiak <wm382710@students.mimuw.edu.pl>
2020-05-22 13:46:55 +0200

    Notes can be clicked to open edit activity

--- NotesTask/app/src/main/java/pl/edu/mimuw/students/wm382710/jnp/task01/EditActivity.kt
+++ NotesTask/app/src/main/java/pl/edu/mimuw/students/wm382710/jnp/task01/EditActivity.kt
@@ -2,12 +2,14 @@ package pl.edu.mimuw.students.wm382710.jnp.task01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_edit.*
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 const val EXTRA_NOTE = "pl.edu.mimuw.students.wm382710.jnp.task01.NOTE"
+const val EXTRA_NOTE_ID = "pl.edu.mimuw.students.wm382710.jnp.task01.NOTE_ID"
 
 class EditActivity : AppCompatActivity() {
     lateinit var note: Note
@@ -18,17 +20,28 @@ class EditActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit)
 
         val rcvNote: Note? = intent.getParcelableExtra(EXTRA_NOTE)
-        requireNotNull(rcvNote)
-        note = rcvNote
-
-        editTitle.setText(note.title)
-        editSummary.setText(note.summary)
-        editContentText.setText(note.content)
+        when {
+            rcvNote !== null -> {
+                setEditedNote(rcvNote)
+            }
+            intent.hasExtra(EXTRA_NOTE_ID) -> {
+                val id = intent.getIntExtra(EXTRA_NOTE_ID, 0)
+                require(id > 0) { "No valid note nor note id in intent" }
+                GlobalScope.launch {
+                    val dao = NoteDatabase.getDatabase(applicationContext).noteDao()
+                    val note = dao.loadNote(id)
+                    MainScope().launch { setEditedNote(note) }
+                }
+            }
+            else -> {
+                throw IllegalArgumentException("No valid note nor note id in intent")
+            }
+        }
     }
 
     override fun onPause() {
         super.onPause()
-        flushNote()
+        flushEditedNote()
         if (noteChanged) {
             GlobalScope.launch {
                 val dao = NoteDatabase.getDatabase(applicationContext).noteDao()
@@ -40,7 +53,7 @@ class EditActivity : AppCompatActivity() {
         }
     }
 
-    private fun flushNote() {
+    private fun flushEditedNote() {
         val title = editTitle.text.toString()
         val summary = editSummary.text.toString()
         val content = editContentText.text.toString()
@@ -48,5 +61,12 @@ class EditActivity : AppCompatActivity() {
         note.title = title
         note.summary = summary
         note.content = content
+    }
+
+    private fun setEditedNote(n: Note) {
+        note = n
+        editTitle.setText(note.title)
+        editSummary.setText(note.summary)
+        editContentText.setText(note.content)
     }
 }
--- NotesTask/app/src/main/java/pl/edu/mimuw/students/wm382710/jnp/task01/MetadataActivity.kt
+++ NotesTask/app/src/main/java/pl/edu/mimuw/students/wm382710/jnp/task01/MetadataActivity.kt
@@ -1,6 +1,7 @@
 
 package pl.edu.mimuw.students.wm382710.jnp.task01
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
@@ -10,32 +11,38 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import java.util.*
 
-class MyAdapter: RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+class MyAdapter(val onclick: (MyViewHolder) -> Unit): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     var dataset: List<NoteMetadata>? = null
 
-    class MyViewHolder(val root: ConstraintLayout): RecyclerView.ViewHolder(root) {
+    class MyViewHolder(val root: ConstraintLayout, val onclick: (MyViewHolder) -> Unit): RecyclerView.ViewHolder(root) {
+        var noteId: Int = -1
         val titleView: TextView = root.findViewById(R.id.textTitle)
         val dateView: TextView = root.findViewById(R.id.textDate)
         val summaryView: TextView = root.findViewById(R.id.textSummary)
+
+        init {
+            root.setOnClickListener { onclick(this) }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
         val textView = LayoutInflater.from(parent.context)
             .inflate(R.layout.metadata_view, parent, false) as ConstraintLayout
-        return MyViewHolder(textView)
+        return MyViewHolder(textView, onclick)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val md = dataset!![position]
+        holder.noteId = md.id
         holder.root.setBackgroundColor(if (position % 2 == 0) Color.GRAY else Color.LTGRAY)
         holder.titleView.text = md.title
         holder.dateView.text = Date(md.modificationDate).toString()
@@ -61,7 +68,15 @@ class MetadataActivity: AppCompatActivity() {
         setContentView(R.layout.activity_metadata)
 
         val layout = LinearLayoutManager(this)
-        viewAdapter = MyAdapter()
+        viewAdapter = MyAdapter { holder ->
+            val context: Context = this
+            GlobalScope.launch {
+                val note = NoteDatabase.getDatabase(applicationContext).noteDao().loadNote(holder.noteId)
+                val intent = Intent(context, EditActivity::class.java)
+                intent.putExtra(EXTRA_NOTE, note)
+                startActivity(intent)
+            }
+        }
         recyclerView = findViewById<RecyclerView>(R.id.metadataView).apply {
             setHasFixedSize(true)
             layoutManager = layout
