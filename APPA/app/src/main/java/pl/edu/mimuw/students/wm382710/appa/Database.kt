9ec6b540e8a16f9a980ce085ee2d697c60ce95e9
Wojciech Matusiak <wm382710@students.mimuw.edu.pl>
2020-06-29 15:55:37 +0200

    Displaying list of available heroes in the main activity

---
+++ APPA/app/src/main/java/pl/edu/mimuw/students/wm382710/appa/Database.kt
@@ -0,0 +1,84 @@
+package pl.edu.mimuw.students.wm382710.appa
+
+import android.content.Context
+import androidx.room.*
+
+@Entity(indices = [Index(value = ["heroName"])])
+data class Hero(
+    @PrimaryKey(autoGenerate = true) val heroId: Int,
+    val heroName: String,
+    val strength: Int = 6,
+    val dexterity: Int = 6,
+    val constitution: Int = 6,
+    val intelligence: Int = 6
+)
+
+@Entity(indices = [Index(value = ["ownerId"])],
+    foreignKeys = [ForeignKey(entity = Hero::class, parentColumns = ["heroId"], childColumns = ["ownerId"], onDelete = ForeignKey.CASCADE)])
+data class Item(
+    @PrimaryKey(autoGenerate = true) val itemId: Int,
+    val ownerId: Int,
+    val name: String,
+    var description: String?
+)
+
+data class HeroWithInventory(
+    @Embedded val hero: Hero,
+    @Relation(
+        parentColumn = "heroId",
+        entityColumn = "itemId"
+    )
+    val playlists: List<Item>
+)
+
+@Entity
+data class AdventureMetadata(
+    @PrimaryKey(autoGenerate = true) val adventureId: Int,
+    val title: String,
+    val description: String?
+)
+
+@Dao
+abstract class AdventureMetadataDao {
+    @Query("SELECT * FROM adventuremetadata WHERE adventureId = :id")
+    abstract fun selectMetadata(id: Int): AdventureMetadata
+
+    @Query("SELECT * FROM adventuremetadata ORDER BY title")
+    abstract suspend fun selectAllMetadata(): List<AdventureMetadata>
+}
+
+@Dao
+abstract class HeroDao {
+    @Transaction
+    @Query("SELECT * FROM hero WHERE heroId = :id")
+    abstract fun selectHeroWithInventory(id: Int): HeroWithInventory
+
+    @Query("SELECT * FROM hero")
+    abstract fun selectAllHeroes(): List<Hero>
+
+    @Insert
+    abstract fun insertHero(hero: Hero)
+
+    @Insert
+    abstract fun updateHero(hero: Hero)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertItems(vararg items: Item)
+}
+
+@Database(entities = [Hero::class, Item::class, AdventureMetadata::class], version = 1)
+abstract class AppaDatabase: RoomDatabase() {
+    companion object {
+        private var database: AppaDatabase? = null
+
+        fun getDatabase(context: Context): AppaDatabase {
+            if (database === null)
+                database = Room.databaseBuilder(context, AppaDatabase::class.java, "notes.db").build()
+            return database!!
+        }
+    }
+
+    abstract fun heroes(): HeroDao
+
+    abstract fun adventures(): AdventureMetadataDao
+}
--- APPA/app/src/main/java/pl/edu/mimuw/students/wm382710/appa/MainActivity.kt
+++ APPA/app/src/main/java/pl/edu/mimuw/students/wm382710/appa/MainActivity.kt
@@ -3,26 +3,114 @@ package pl.edu.mimuw.students.wm382710.appa
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import java.io.File
 import java.io.FileOutputStream
 import java.util.zip.ZipFile
 
 
-const val PICK_ADVENTURE_ZIP = 1
+class HeroesListAdapter(private val activity: MainActivity): RecyclerView.Adapter<HeroesListAdapter.ViewHolder>() {
+
+    var dataset: List<Hero>? = null
+
+    class ViewHolder(root: View, private val activity: MainActivity): RecyclerView.ViewHolder(root) {
+        private var myHero: Hero? = null
+        private val nameView: TextView = root.findViewById(R.id.heroNameText)
+        private val strengthView: TextView = root.findViewById(R.id.strengthText)
+        private val intelligenceView: TextView = root.findViewById(R.id.intelligenceText)
+        private val dexterityView: TextView = root.findViewById(R.id.dexterityText)
+        private val constitutionView: TextView = root.findViewById(R.id.constitutionText)
+
+        init {
+            root.findViewById<ImageButton>(R.id.deleteHeroButton).setOnClickListener { activity.onHeroDeleted(hero) }
+            nameView.setOnClickListener { activity.onHeroSelected(hero) }
+        }
+
+        var hero: Hero
+            get() = myHero ?: throw IllegalStateException("No hero was assigned yet to this view")
+            set(h) {
+                myHero = h
+                nameView.text = h.heroName
+                strengthView.text = h.strength.toString()
+                intelligenceView.text = h.intelligence.toString()
+                dexterityView.text = h.dexterity.toString()
+                constitutionView.text = h.constitution.toString()
+            }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.hero_summary, parent, false)
+        return ViewHolder(view, activity)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.hero = dataset!![position]
+    }
+
+    override fun getItemCount() = if (dataset !== null) dataset!!.size else 0
+
+    var data: List<Hero>
+        get() = dataset ?: ArrayList()
+        set(value) {
+            dataset = value
+            MainScope().launch { notifyDataSetChanged() }
+        }
+}
+
 
 class MainActivity : AppCompatActivity() {
+    val adapter = HeroesListAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.createHeroButton).setOnClickListener { onHeroCreationRequested() }
+        findViewById<RecyclerView>(R.id.heroesRecyclerView).apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = this@MainActivity.adapter
+        }
+
+        GlobalScope.launch {
+            val db = AppaDatabase.getDatabase(applicationContext)
+            adapter.data = db.heroes().selectAllHeroes()
+        }
+    }
+
+    fun onHeroCreationRequested() {
+        GlobalScope.launch {
+            val hero = Hero(0, "X the D")
+            val db = AppaDatabase.getDatabase(applicationContext)
+            db.heroes().insertHero(hero)
+            adapter.data = db.heroes().selectAllHeroes()
+        }
+    }
+
+    fun onHeroDeleted(hero: Hero) {
+
+    }
+
+    fun onHeroSelected(hero: Hero) {
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
             uri?.let { openFile(it) }
         }.launch("application/zip")
     }
 
-    fun openFile(uri: Uri) {
+    private fun openFile(uri: Uri) {
         val copyName = "test.zip"
         val inStream = contentResolver.openInputStream(uri)
         val zipFile = File(cacheDir.path, copyName)
@@ -41,11 +129,10 @@ class MainActivity : AppCompatActivity() {
         startAdventure(zipFile.path)
     }
 
-    fun startAdventure(zipFileName: String) {
+    private fun startAdventure(zipFileName: String) {
         val intent = Intent(this, AdventureActivity::class.java)
         intent.putExtra("zipFileName", zipFileName)
         startActivity(intent)
         finish()
     }
-
 }
